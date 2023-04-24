334159bee2150a1df2b2479fc374f5beb119d1fe
JoaoCardoso00 <jvcq13@gmail.com>
2023-04-24 11:19:13 -0300

    feat: add dynamic routes page

---
+++ src/utils/routes.ts
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    name: 'Praia vai quem quer - 23 mar',
+    slug: 'praia-vai-quem-quer',
+    time: '4 a 5 Horas',
+    experience: [
+      'Alimentação inclusa',
+      'Transporte incluso',
+      'Vendas de bebidas',
+    ],
+    fullPrice: 1756,
+    priceWithDiscount: 983,
+    priceInstallmentsNumber: 12,
+    image: '/img/vai-quem-quer.webp',
+    imageAlt: 'Praia vai quem quer',
+    tags: ['Cotico'],
+  },
+];
